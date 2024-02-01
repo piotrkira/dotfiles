387ae178f5b8d38a1b377382f9369ef6cdd219e5
Piotr Kira <piotr@kira.pl>
2024-02-01 19:14:34 +0100

    nvim: remove languagetool linting

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -108,7 +108,6 @@ return {
     config = function()
       local lint = require("lint")
       lint.linters_by_ft = {
-        markdown = { "languagetool" },
         sh = { "shellcheck" },
       }
       vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
